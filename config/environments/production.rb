016f3ad875924182088c31413af3ea8496d4cebf
nebulis33 <>
2021-03-10 13:50:21 -0800

    changed host url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'smtp.sendgrid.net' }
+  config.action_mailer.default_url_options = { host: 'https://secure-reaches-90548.herokuapp.com' }
   config.action_mailer.smtp_settings = {
     :user_name            => 'apikey',
     :password             => Rails.application.credentials.sendgrid[:api_key],
