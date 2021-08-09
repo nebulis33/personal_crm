30dadbba9714e438e102959c5e22d3014d86b2be
nebulis33 <>
2021-08-09 15:20:20 -0700

    minor view work before working on calendar

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -13,7 +13,7 @@ class ContactsController < ApplicationController
         @last_contact_type = @contact.last_contact ? @contact.last_contact.interaction_type : 'No recent contact!'
         @last_contact_date = @contact.last_contact ? @contact.last_contact.date.strftime("%m/%d/%Y") : ''
 
-        @upcoming_event = @contact.events.upcoming_events ? @contact.events.upcoming_events.first : nil
+        @upcoming_events = @contact.events.upcoming_events ? @contact.events.upcoming_events.first(3) : nil
         @past_events = @contact.events.recent_events.limit(5)
     end
 
